bb19d95195626e06d14feb7e8b165daeeb1e9565
amal.pj <amal.pj@germinait.com>
2024-05-29 13:01:24 +0530

    intitial commit

---
+++ src/main/java/com/germin8/whatsapp_subscriber/WhatsappSubscriberApplication.java
@@ -0,0 +1,13 @@
+package com.germin8.whatsapp_subscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappSubscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappSubscriberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/germin8/whatsapp_subscriber/WhatsappSubscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germin8.whatsapp_subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappSubscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
